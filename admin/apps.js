b4be824d2ced82689f26435e8a6b19a6d95b9a69
Rezafaghih <royalgaming.work@gmail.com>
2023-03-05 17:06:09 -0800

    apps.js added

--- admin/apps.js
+++ admin/apps.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function(){
     var button = this.document.querySelectorAll(".navbar-buttons button");
-  
-  
+    var container = this.document.querySelectorAll("#real-box-container");
     for (let index = 0; index < button.length; index++) {
       button[index].addEventListener("click", function(){
         for (let i = 0; i < button.length; i++) {
@@ -10,6 +9,10 @@ window.addEventListener("load", function(){
           }
         }
         button[index].setAttribute("act", '1');
+        for (let i = 0; i < container.length; i++) {
+            container[i].style.display="none";
+          }
+          container[index].style.display="flex";
       })
     }
   })
