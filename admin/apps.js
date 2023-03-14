b31cda50bde354f473a0fc68baf156242d84a562
Rezafaghih <royalgaming.work@gmail.com>
2023-03-14 04:04:15 +0330

    admin panel updated

--- admin/apps.js
+++ admin/apps.js
@@ -1,23 +1,4 @@
 window.addEventListener("load", function(){
-<<<<<<< HEAD
-    var button = this.document.querySelectorAll(".navbar-buttons button");
-    var container = this.document.querySelectorAll("#real-box-container");
-    for (let index = 0; index < button.length; index++) {
-      button[index].addEventListener("click", function(){
-        for (let i = 0; i < button.length; i++) {
-          if (button[i].hasAttribute("act")){
-            button[i].removeAttribute('act');
-          }
-        }
-        button[index].setAttribute("act", '1');
-        for (let i = 0; i < container.length; i++) {
-            container[i].style.display="none";
-          }
-          container[index].style.display="flex";
-      })
-    }
-  })
-=======
   var nav_buttons = document.querySelectorAll("nav button");
   if (nav_buttons != undefined){
     for (let index = 0; index < nav_buttons.length; index++) {
@@ -31,4 +12,3 @@ window.addEventListener("load", function(){
   }
 })
 
->>>>>>> a27eef6fbb49e4c17752cc70a7e3d1b4078332b9
